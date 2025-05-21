df0fa7c95d048aface7bba2205350820dc7ef86a
romariomartinez <raldairmartinez@unicesar.edu.co>
2025-05-21 18:51:43 -0500

    decimo commit

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -11,16 +11,16 @@ const SplashScreen = ({ onComplete }) => {
   return (
     <div className="h-screen w-screen bg-white flex items-center justify-center overflow-hidden">
       <motion.video
-        src="/videos/VIDEOSPLAS.mp4"
-        autoPlay
-        muted
-        loop
-        playsInline
-        className="w-full h-full object-cover sm:object-contain"
-        initial={{ scale: 0.9, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ type: "spring", duration: 1.5, bounce: 0.4 }}
-      />
+  src="/videos/VIDEOSPLAS.mp4"
+  autoPlay
+  muted
+  loop
+  playsInline
+  className="max-w-full max-h-screen object-contain p-4"
+  initial={{ scale: 0, opacity: 0 }}
+  animate={{ scale: 1, opacity: 1 }}
+  transition={{ type: "spring", duration: 1.5, bounce: 0.5 }}
+/>
     </div>
   );
 };
